598ad776763e58972938d208955047e5d216765b
Mohamed <mohamed@MacBook-Air-de-Mohamed.local>
2024-10-14 17:44:22 +0200

    ajout sidebar

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ import duckdb
 import streamlit as st
 import io
 
-
 csv='''
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
 '''
-
 beverages=pd.read_csv(io.StringIO(csv))
 
 csv2='''
@@ -19,15 +17,20 @@ cookie juice,2.5
 chocolatine,2
 muffin,3
 '''
-
 food_items=pd.read_csv(io.StringIO(csv2))
 
 answer = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
-
 solution = duckdb.query(answer).df()
+
+with st.sidebar:
+    option=st.selectbox("What would you ike to review ?",
+                        ("Joins","GroupBy","Windows Functions"),
+                        placeholder='Select a theme...',
+                        index=None)
+    st.write(f"You selected : {option}")
 
 
 st.header("entrer votre code : ")
