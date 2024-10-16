9c4137cf883e8ef3ea297a0aec761ece37440aa5
Mohamed <mohamed@MacBook-Air-de-Mohamed.local>
2024-10-16 19:47:45 +0200

    comparaison with real answer

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-# solution_df = duckdb.query(ANSWER_STR).df()
-
 with st.sidebar:
     option = st.selectbox(
         "What would you ike to review ?",
@@ -20,34 +18,39 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state where theme = '{option}'").df()
     if not exercise.empty:
         st.dataframe(exercise)
+        exercise_answer = exercise.loc[0, "answer"]
+        with open(f"answer/{exercise_answer}", "r", encoding="utf-8") as f:
+            answer = f.read()
+            solution_df = con.execute(answer).df()
 
 
 st.header("entrer votre code : ")
 query = st.text_area(label="Votre code SQL ici", key="user_input")
 
 if query:
-    result = con.execute(query)
+    result = con.execute(query).df()
     st.dataframe(result)
-#    try:
-#        result = result[solution_df.columns]
-#        st.dataframe(result.compare(solution_df))
-#    except KeyError as e:
-#        st.write(f"Erreur de colonne : {e}")
-#        liste = []
-#        diff_lin = solution_df.shape[0] - result.shape[0]
-#        for columns in solution_df.columns:
-#            if columns not in result.columns:
-#                liste.append(columns)
-#
-#        if liste:
-#            st.write("il manque les colonnes suivantes : ")
-#            for columns in liste:
-#                st.write(f"-------{columns}")
-#        elif diff_lin != 0:
-#            st.write(f"il y a un écart de {diff_lin} lignes")
-#
-#        else:
-#            st.write("Veuillez remonter cette erreur au support : support@support.com ")
+    try:
+        result = result[solution_df.columns]
+        check_df = result.compare(solution_df)
+        if not check_df.empty:
+            st.dataframe()
+    except KeyError as e:
+        liste = []
+        diff_lin = solution_df.shape[0] - result.shape[0]
+        for columns in solution_df.columns:
+            if columns not in result.columns:
+                liste.append(columns)
+
+        if liste:
+            st.write("il manque les colonnes suivantes : ")
+            for columns in liste:
+                st.write(f"-------{columns}")
+        elif diff_lin != 0:
+            st.write(f"il y a un écart de {diff_lin} lignes")
+
+        else:
+            st.write("Veuillez remonter cette erreur au support : support@support.com ")
 
 tab2, tab3 = st.tabs(("Tables", "Solution"))
 
@@ -61,7 +64,4 @@ with tab2:
 
 with tab3:
     if not exercise.empty:
-        exercise_answer = exercise.loc[0, "answer"]
-        with open(f"answer/{exercise_answer}", "r", encoding="utf-8") as f:
-            answer = f.read()
-            st.write(answer)
+        st.write(answer)
